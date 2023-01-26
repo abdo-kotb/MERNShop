385b0e1c2c3a9783620b9b74c6d112432740e7b9
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-01-26 15:20:40 +0400

    finish rating component

--- client/components/rating.tsx
+++ client/components/rating.tsx
@@ -9,39 +9,49 @@ import { faStar } from '@fortawesome/free-regular-svg-icons'
 interface IProps {
   value: number
   text: string
+  color: string
 }
 
-const Rating: FC<IProps> = ({ value, text }) => {
+const Rating: FC<IProps> = ({ value, text, color }) => {
   return (
     <div className="rating">
       <span>
         <FontAwesomeIcon
+          color={color}
           icon={value >= 1 ? fullStar : value >= 0.5 ? faStarHalfAlt : faStar}
         />
       </span>
       <span>
         <FontAwesomeIcon
+          color={color}
           icon={value >= 2 ? fullStar : value >= 1.5 ? faStarHalfAlt : faStar}
         />
       </span>
       <span>
         <FontAwesomeIcon
+          color={color}
           icon={value >= 3 ? fullStar : value >= 2.5 ? faStarHalfAlt : faStar}
         />
       </span>
       <span>
         <FontAwesomeIcon
+          color={color}
           icon={value >= 4 ? fullStar : value >= 3.5 ? faStarHalfAlt : faStar}
         />
       </span>
       <span>
         <FontAwesomeIcon
+          color={color}
           icon={value >= 5 ? fullStar : value >= 4.5 ? faStarHalfAlt : faStar}
         />
       </span>
-      {text && <span>{text}</span>}
+      {text && <span className="ms-1">{text}</span>}
     </div>
   )
+}
+
+Rating.defaultProps = {
+  color: '#f8e825',
 }
 
 export default Rating
