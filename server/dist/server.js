cf2e377d24141832d3bca4a3a4f7fd51e6588a26
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-01-26 18:52:35 +0400

    finish initial backend setup

---
+++ server/dist/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import products from './data/products.js';
+const app = express();
+app.get('/', (req, res) => {
+    res.send('API running');
+});
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+app.get('/api/product/:id', (req, res) => {
+    const { params: { id }, } = req;
+    const product = products.find(product => product._id === id);
+    res.json(product);
+});
+app.listen(5000, () => console.log('Server running on port 5000'));
+//# sourceMappingURL=server.js.map
