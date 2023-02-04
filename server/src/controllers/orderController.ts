810326b7c8b5242b7f92dca88e73f1f3e9ddebf6
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-02-04 21:49:53 +0400

    create payment routes

--- server/src/controllers/orderController.ts
+++ server/src/controllers/orderController.ts
@@ -37,4 +37,19 @@ const addOrderItems = asyncHandler(async (req: UserReq, res) => {
   res.status(201).json(createdOrder)
 })
 
-export { addOrderItems }
+// @desc Get order by ID
+// @route GET /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+
+  if (order) res.json(order)
+
+  res.status(404)
+  throw new Error('Order not found')
+})
+
+export { addOrderItems, getOrderById }
