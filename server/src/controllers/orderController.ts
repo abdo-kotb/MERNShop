fdbb0078f1708d6c7173c35c75ed1b0fd1fa3c61
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-02-04 22:53:34 +0400

    add update order status route

--- server/src/controllers/orderController.ts
+++ server/src/controllers/orderController.ts
@@ -52,4 +52,29 @@ const getOrderById = asyncHandler(async (req, res) => {
   throw new Error('Order not found')
 })
 
-export { addOrderItems, getOrderById }
+// @desc Update order to paid
+// @route GET /api/orders/:id/pay
+// @access Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isPaid = true
+    order.paidAt = new Date(Date.now())
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.email_address,
+    }
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  }
+
+  res.status(404)
+  throw new Error('Order not found')
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid }
