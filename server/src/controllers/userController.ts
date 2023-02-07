51ad6c1e004a4d69368be273dba6928eef20e956
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-02-08 00:52:25 +0400

    create deleteUser endpoint

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -110,10 +110,32 @@ const updateUserProfile = asyncHandler(async (req: UserReq, res) => {
 // @desc Get all users
 // @route GET /api/users
 // @access Private/Admin
-const getUsers = asyncHandler(async (req: UserReq, res) => {
+const getUsers = asyncHandler(async (_, res) => {
   const users = await User.find({})
 
   res.json(users)
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
+// @desc Delete user
+// @route DELETE /api/users/:id
+// @access Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    await user.remove()
+    res.json({ message: 'User removed' })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+}
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import {
   authUser,
+  deleteUser,
   getUserProfile,
   getUsers,
   registerUser,
@@ -16,5 +17,6 @@ router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile)
+router.route('/:id').delete(protect, isAdmin, deleteUser)
 
 export default router
