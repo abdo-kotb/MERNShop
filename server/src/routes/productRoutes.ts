32b5c533baabe3cc0fa4e4ab65abc8ef7609e581
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-02-08 23:17:54 +0400

    add createProduct & updateProduct endpoints

--- server/src/routes/productRoutes.ts
+++ server/src/routes/productRoutes.ts
@@ -1,15 +1,21 @@
 import express from 'express'
 import {
+  createProduct,
   deleteProduct,
   getProductById,
   getProducts,
+  updateProduct,
 } from '../controllers/productController.js'
 import { isAdmin, protect } from '../middelwares/authMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, isAdmin, createProduct)
 
-router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, isAdmin, deleteProduct)
+  .put(protect, isAdmin, updateProduct)
 
 export default router
