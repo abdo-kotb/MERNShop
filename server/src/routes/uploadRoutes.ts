a7dbc411429da223913d1e703cc1409ab06d548e
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-02-09 19:35:24 +0400

    add file upload configuration

---
+++ server/src/routes/uploadRoutes.ts
@@ -0,0 +1,45 @@
+import express from 'express'
+import multer from 'multer'
+import path from 'path'
+
+const router = express.Router()
+
+const storage = multer.diskStorage({
+  destination(_, __, cb) {
+    cb(null, 'uploads/')
+  },
+  filename(_, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    )
+  },
+})
+
+const checkFileType = (
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
+  const fileTypes = /jpg|jpeg|png/
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
+  const mimetype = fileTypes.test(file.mimetype)
+
+  if (extname && mimetype) {
+    return cb(null, true)
+  } else {
+    cb(new Error('Images only'))
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: (_, file, cb) => {
+    checkFileType(file, cb)
+  },
+})
+
+router.post('/', upload.single('image'), (req, res) => {
+  res.send(`/${req.file.path}`)
+})
+
+export default router
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,9 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import { errorHandler, notFound } from './middelwares/errorMiddleware.js'
+import path from 'path'
 
 colors.enable()
 
@@ -28,10 +30,13 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
