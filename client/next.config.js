fe3306b8dba6739eeaedef01d4c2aa9f639c84ec
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-02-13 16:25:14 +0400

    deploy client

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    API_ROOT: 'http://localhost:5000/api',
-  },
 }
 
 module.exports = nextConfig
