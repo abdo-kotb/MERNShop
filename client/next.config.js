80a5cb5f5a8617f51c2af8e3084f5102afe0dc2a
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-02-18 18:02:54 +0400

    fix url

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_ROOT: 'https://mern-shop-1xoh.onrender.com/',
+  },
 }
 
 module.exports = nextConfig
